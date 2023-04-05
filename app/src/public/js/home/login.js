d7b2263a9db581c324ae976a8e72727fadd81a7f
LeeHYn <jemie9812@gmail.com>
2023-04-05 10:46:08 +0900

    DOM을 이용하요 저장소에 데이터 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,15 @@
 "use static";
 
-console.log("hi");
-console.log("bye");
+const id = document.querySelector("#id");
+psw = document.querySelector("#pwd");
+loginbtn = document.querySelector("button");
+
+loginbtn.addEventListener("click", login);
+
+function login() {
+  const req = {
+    id: id.value,
+    pwd: pwd.value,
+  };
+  console.log(req);
+}
