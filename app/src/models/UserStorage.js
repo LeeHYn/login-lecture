71d02ab52700eab83fe4a7aea3c7aaa9fb96af80
LeeHYn <jemie9812@gmail.com>
2023-04-06 11:25:59 +0900

    서버가 재가동하기 전까지만 기동하는 회원가입 기능 구현 더 구현 필요

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -27,6 +27,12 @@ class UserStorage {
     }, {});
     return userInfo;
   }
+  static save(userInfo) {
+    const users = this.#users;
+    users.id.push(userInfo.id);
+    users.name.push(userInfo.name);
+    users.pwd.push(userInfo.pwd);
+  }
 }
 
 module.exports = UserStorage;
